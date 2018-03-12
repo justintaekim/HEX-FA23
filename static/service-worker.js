a7f5c16075e050224ed122c511cc08df948d825e
Ethan Lee <hello@ethanl.ee>
2018-03-11 21:12:52 -0700

    Service worker update

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,18 @@
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-  for (let registration of registrations) {
-    registration.unregister();
-  } 
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', () => {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    } 
+  });
+  
+  self.clients.matchAll({type: 'window'}).then(windowClients => {
+    windowClients.forEach(windowClient => {
+      windowClient.navigate(windowClient.url);
+    });
+  });
+});
+

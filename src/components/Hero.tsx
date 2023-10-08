bf71838460df6c65ac7dc924f59a09b0c6060042
justintaekim <justinkim607@berkeley.edu>
2023-10-08 15:39:27 -0700

    Made the layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import heroStyles from "./Hero.css";
+import heroStyles from "./Hero.module.css";
+
+// This file will have the navbar and the Hero
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className={heroStyles.hero}>
       {/* NavBar */}
       <div>Innovative Design</div>
       <p>asldkjflaskdnvlkdasjflkdasnlkfnakfjsalkjfaldkjafdlskdj</p>

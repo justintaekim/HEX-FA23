0044451f3756b94de4da1967d80811596e9ef378
justintaekim <justinkim607@berkeley.edu>
2023-10-08 15:20:01 -0700

    Lets start

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import heroStyles from "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      {/* NavBar */}
+      <div>Innovative Design</div>
+      <p>asldkjflaskdnvlkdasjflkdasnlkfnakfjsalkjfaldkjafdlskdj</p>
+    </div>
+  );
+};
+
+export default Hero;
